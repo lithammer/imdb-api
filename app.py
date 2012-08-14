1162326e8234516d93754d9140feffb2d7648af0
Peter Renström <renstrom.peter@gmail.com>
2012-08-14 17:42:30 +0200

    Add static routing

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, Response, render_template, abort, jsonify
 from bs4 import BeautifulSoup
 import requests
 
-app = Flask(__name__)
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+app = Flask(__name__, static_folder=os.path.join(PROJECT_ROOT, 'public'), static_url_path='/public')
 
 
 @app.errorhandler(404)
