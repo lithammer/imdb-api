287a20775e9b2a5ddd47456df62a2ee825a36784
Peter Renström <renstrom.peter@gmail.com>
2012-09-07 11:25:09 +0200

    Remove superflous static files settings. I was only setting the same values as the default ones anyway.

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ from bs4 import BeautifulSoup
 from settings import HOST, DEBUG
 from filters import support_jsonp, cached
 
-PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-app = Flask(__name__, static_folder=os.path.join(PROJECT_ROOT, 'static'),
-        static_url_path='/static')
 
 
 @app.errorhandler(404)
