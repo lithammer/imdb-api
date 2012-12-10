3d22c4f290734201fd8466a18c529c82c33a0660
Peter Renström <renstrom.peter@gmail.com>
2012-12-10 16:40:30 +0100

    Support full IMDB URLs in the search field.

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ function AppCtrl($scope, $http) {
 		$scope.loading = true;
 		$scope.movie = null;
 
-		$http.get('/search/' + $scope.imdbId).then(function(response) {
+		$http.get('/search/' + $scope.imdbId.match(/tt[\d]{7}/)[0]).then(function(response) {
 			$scope.loading = false;
 			$scope.movie = response.data;
 		});
