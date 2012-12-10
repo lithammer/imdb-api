a2df92d456708789a1b7bbeb90c091e0e6236b04
Peter Renström <renstrom.peter@gmail.com>
2012-12-10 14:48:29 +0100

    Reworked to scrape the mobile site and use Angularjs instead of jQuery.
    - The mobile site is about 85kb smaller than the regular site (19kb vs.
      104kb).
    - Angularjs because I like Angularjs :)
    - Also removed the bootstrap javascript files.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import datetime
 import os
 import re
 import requests
@@ -36,8 +37,8 @@ def proxy_image(file):
 @support_jsonp
 @cached
 def search(id, width=200, height=296):
-    """ Fetches and scrapes a movie or tv show by imdb id """
-    html = requests.get('http://www.imdb.com/title/{}/'.format(id))
+    """ Fetches and scrapes a movie by imdb id """
+    html = requests.get('http://m.imdb.com/title/{}/'.format(id))
     host = request.headers['Host']
 
     if not html.ok:
@@ -48,32 +49,35 @@ def search(id, width=200, height=296):
     except:
         return jsonify(error='Error occured while parsing.')
 
-    items = soup.find_all(itemprop=True)
-
-    poster = soup.find(rel='image_src')['href']
-    # Change poster size
-    poster = re.sub(r'@@.+', '@@.SX{}_SY{}.jpg'.format(
-        width, height), poster)
+    poster = soup.find_all('img')[1].attrs['src']
+    poster = re.sub(r'@@.+', '@@.SX{width}_SY{height}.jpg'.format(
+        width=width, height=height), poster)
     poster = poster.rpartition('/')[2]
     poster = 'http://{}/poster/{}'.format(host, poster)
 
-    title = list(items[1].strings)[0].strip()
-    description = items[8].text.strip()
-    vote_count = int(items[5].text.replace(',', ''))
-    vote_average = float(items[3].text)
+    title = re.match(r'[\w\s]+\w', soup.title.string).group(0)
+    vote_average = float(soup.strong.string)
 
-    year = soup.time['datetime']
+    vote_count = soup.find_all('p', {'class': 'votes'})[0].contents[2]
+    vote_count = re.search(r'\(([\d,]+)', vote_count)
+    vote_count = int(vote_count.groups()[0].replace(',', ''))
 
-    genres = []
-    for g in soup.find_all(itemprop='genre'):
-        genres.append(g.strings.next())
+    details = soup.find('section', {'class': 'details'}).find_all('p')
+
+    release_date = datetime.datetime.strptime(details[0].string, ' %d %b %Y')
+    release_date = '{:%Y-%m-%d}'.format(release_date)
+
+    genres = details[1].string.split(', ')
+    run_time = details[2].string
+    description = details[4].contents[0].strip()
 
     return jsonify(poster=poster,
                    title=title,
-                   release_date=year,
+                   release_date=release_date,
                    overview=description,
                    vote_count=vote_count,
                    vote_average=vote_average,
+                   run_time=run_time,
                    genres=genres)
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,32 +1,11 @@
-$('form').submit(function(event) {
-	event.preventDefault();
+function AppCtrl($scope, $http) {
+	$scope.search = function() {
+		$scope.loading = true;
+		$scope.movie = null;
 
-	var movie = $('#movie'),
-		id = $('#search-id').val();
-
-	movie.html('<img src="/static/img/loading.gif">');
-
-	var request = $.getJSON('/search/' + id + '/100', function(data) {
-		var html = '';
-		html += '<img src="' + data.poster + '" class="img-polaroid">';
-		html += '<section class="movie-info">';
-		html += '<h2>' + data.title + '</h2>';
-		html += '<p>Genres: ';
-		$.each(data.genres, function(index, value) {
-			html += value;
-			if (data.genres[index + 1]) {
-				html += ' / ';
-			}
+		$http.get('/search/' + $scope.imdbId).then(function(response) {
+			$scope.loading = false;
+			$scope.movie = response.data;
 		});
-		html += '</p>';
-		html += '<p>Rating: ' + data.vote_average + ' (' + data.vote_count + ' votes)</p>';
-		html += '<p>Release date: ' + data.release_date + '</p>';
-		html += '<p>' + data.overview + '</p>';
-		html += '</section>';
-		movie.html(html);
-	});
-
-	request.fail(function(jqXHR, textStatus) {
-		movie.html('<div class="alert alert-error">Bad request!</div>');
-	});
-});
+	};
+}
